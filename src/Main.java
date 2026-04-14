db41aec9f95eda5d7dd8cc557b6cc521de8fd7dc
iLuvMelon <dylankane0117@gmail.com>
2026-04-14 10:38:41 -0400

    refined

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,10 @@ public class Main {
     public static void main(String[] args) {
         MacBook myMac = new MacBook();
 
-        // 2. Create the OldMonitor (The Adaptee)
         OldMonitor oldDisplay = new OldMonitor();
 
-        // 3. Create the Adapter and plug the monitor into it
         Adapter adapter = new Adapter(oldDisplay);
 
-        // 4. Connect the MacBook to the adapter
-        // The MacBook thinks it's talking to a Type-C device!
         myMac.connectToTypeCDevice(adapter);
     }
 }
